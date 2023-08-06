5cae0a0fe0b1532b43791658ea4185e8f0fa2159
Castilo Farmisi <141524234+Castilo9009@users.noreply.github.com>
2023-08-07 01:26:54 +0300

    testing files included and changed
    new files uploaded with commit 291

---
+++ UnitTestingWebAPI/UnitTestingWebAPI.API/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
+using Owin;
+using UnitTestingWebAPI.API.Controllers;
+using UnitTestingWebAPI.API.MessageHandlers;
+using UnitTestingWebAPI.API.MediaTypeFormatters;
+using UnitTestingWebAPI.Data.Infrastructure;
+using UnitTestingWebAPI.Data.Repositories;
+using UnitTestingWebAPI.Services;
+using HeaderAppender = UnitTestingWebAPI.API.HeaderAppenderHandler;
+using UnitTestingWebAPI.API.Filters;
+
+[assembly: OwinStartup(typeof(UnitTestingWebAPI.API.Startup))]
+
+namespace UnitTestingWebAPI.API
+{
+    public class Startup
+    {
+        //https://chsakell.com/2015/05/10/asp-net-web-api-unit-testing/
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            var config = new HttpConfiguration();
+            config.Services.Replace(typeof(IAssembliesResolver), new CustomAssembliesResolver());
+            config.MessageHandlers.Add(new HeaderAppender.HeaderAppenderHandler());
+            config.MessageHandlers.Add(new EndRequestHandler());
+            config.Filters.Add(new ArticlesReversedFilter());
+            config.Formatters.Add(new ArticleFormatter());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Autofac configuration
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(typeof(BlogsController).Assembly);
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            //Repositories
+            builder.RegisterAssemblyTypes(typeof(BlogRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+            // Services
+            builder.RegisterAssemblyTypes(typeof(ArticleService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            IContainer container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
